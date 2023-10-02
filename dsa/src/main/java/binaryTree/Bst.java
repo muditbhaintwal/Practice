5e9c02545d7d9295fa67b3b1fdb175e5ae68723f
mudit bhaintwal <mudit.bhaintwal@gmail.com>
2023-10-02 13:25:38 +0530

    restructuring2

---
+++ dsa/src/main/java/binaryTree/Bst.java
@@ -0,0 +1,15 @@
+package binaryTree;
+
+/*
+BST is a type of Binary tree which has following properties
+1- Left nodes have value less than root nodes
+2- right nodes have values greater than root node
+3- Left and Right subtree of a BST are also BST
+*
+ */
+
+public class Bst {
+    public static void main(String[] args) {
+        System.out.println("Hello from BST");
+    }
+}

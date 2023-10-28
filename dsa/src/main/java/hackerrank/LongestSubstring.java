e8be3470d7b6feec974de6a4e109b7f8a9ddcf42
mudit <mudit.bhaintwal@gmail.com>
2023-10-28 12:02:27 +0530

    LongestSubstring

--- dsa/src/main/java/hackerrank/LongestSubstring.java
+++ dsa/src/main/java/hackerrank/LongestSubstring.java
@@ -1,7 +1,7 @@
 package hackerrank;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.awt.datatransfer.StringSelection;
+import java.util.*;
 
 /*
 Given a string s, find the length of the longest substring without repeating characters.
@@ -11,6 +11,7 @@ Example 1:
 Input: s = "abcabcbb"
 Output: 3
 Explanation: The answer is "abc", with the length of 3.
+
 Example 2:
 
 Input: s = "bbbbb"
@@ -25,18 +26,38 @@ public class LongestSubstring {
     }
 
     private static void findLongestSubstring(String str) {
+        int start = 0;
+        int end = 0;
+
+        Map<Integer, List<String>> mapOfSubstrings = new HashMap<>();
+
+        getAllSubstrings(str);
+
+
+        for (int i = 0; i < str.length(); i++) {
+
+            char c = str.charAt(i);
+
+        }
+
         String longestSubStr = "";
         Set set = new HashSet(str.length());
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
             if (set.contains(c)) {
-                if(1==1){
+                if (1 == 1) {
 
                 }
                 break;
             }
             set.add(c);
         }
+    }
+
+    private static List<String> getAllSubstrings(String str) {
+        List<String> list = new ArrayList<>();
+
+        return list;
     }
 
 }
